0018a104cd88f24a99ff8caf5eb666d64ab8d941
wagnermarques <wagnerdocri@gmail.com>
2021-01-30 20:40:38 -0300

    playbook-container-docker-postgresql roda o container atraves do docker-run.sh passando parametros para este script

--- playbook-container-docker-postgresql/run-ansible-playbook.sh
+++ playbook-container-docker-postgresql/run-ansible-playbook.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd $(dirname $0)
+thisScriptDir=$(dirname $0)
+cd $thisScriptDir
+
 docker stop fzl_postgresql; docker rm fzl_postgresql
 
 python3 $(which ansible-playbook) -i ./hosts site.yml -e "@extra-vars.json" --ask-become-pass
